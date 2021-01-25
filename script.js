e1f90555abf555c00f723952976ab716e3d0c32d
salsberyscottj <salsberyscottj@johndeere.com>
2021-01-25 05:10:03 -0600

    partially working search counter

--- script.js
+++ script.js
@@ -20,62 +20,81 @@ function layMines() {
             mineField[i][j] = squares[divCount];
             mineField[i][j].dataset.isMine = rando;
             mineField[i][j].dataset.neighbMines = 0;
+            mineField[i][j].dataset.row = i;
+            mineField[i][j].dataset.column = j;
             mineField[i][j].setAttribute('onClick','play(this)');
-            //console.log(mineField[i][j]);
             divCount++;
         }
     }
-
 }
 layMines();
 
-// function neighborSearch() {
-//     //console.log(mineField[0][0].dataset.isMine)
-//     for(let i=0;i<fieldDim;i++) {
-//         for(let j=0;j<fieldDim;j++) {
-//             for(let m=i-1;m<=i+1;m++) {
-//                 for(let n=j-1;n<=j+1;j++){
-//                     console.log(m, n);
-//                     if(mineField[m][n].dataset.isMine === '1'){
-//                         mineField[i][j].dataset.neighbMines = parseInt(mineField[i][j].dataset.neighbMines,10) + 1;
-//                     }
-//                 }
-//             }
-//         }
-//     }
+// console.log(m, n);
+// if(mineField[m][n].dataset.isMine === '1'){
+//     mineField[i][j].dataset.neighbMines = parseInt(mineField[i][j].dataset.neighbMines,10) + 1;
 // }
-// neighborSearch();
 
+function searchNeighbors(i,j) {
+    for(let m=i-1;m<=i+1;m++) {
+        for(let n=j-1;n<=j+1;n++){
+            console.log(i,j,m,n);
+            pickyLook(i,j,m,n);
+        }
+    }
+}
 
-function ckNeighb(i,j,m,n) {
+function pickyLook (i,j,m,n) {
+    if (i > 0 || m >= i) {
+        console.log("i is greater than 0 or m is greater-than or equal to i");
+        if (i < fieldDim || m <= i) {
+            console.log('i is less than fielddim or m is less than or equal to i');
+            if (j > 0 || n >= j) {
+                console.log('j is greater than 0 or n is greater than or equal to j');
+                if (j < fieldDim || n <= j) {
+                    console.log('j is less than fielddim or n is less than or equal to j');
+                    if (m !== i || n !== j) {
+                        console.log('m is not equal to i or n is not equal to j');
+                        checkNeighbor(i,j,m,n);
+                    }
+                }
+            }
+        }
+    }
+}
+
+function checkNeighbor(i,j,m,n) {
     if(mineField[m][n].dataset.isMine === '1'){
         addValue(i,j)
     }
-    console.log(mineField[i][j]);
 }
-// ckNeighb();
 
 function addValue(i,j) {
-    mineField[i][j].dataset.neighbMines = parseInt(mineField[i][j].dataset.neighbMines,10) + 4;
+    mineField[i][j].dataset.neighbMines = parseInt(mineField[i][j].dataset.neighbMines,10) + 1;
 }
 
 function reset() {
     divCount = 0;
     squares.forEach(function(element) {
         element.style.backgroundColor = 'yellow';
+        element.innerText = '';
     })
     layMines();
 }
 
 function play(element) {
-    if (element.dataset.isMine === '0') {
-        element.style.backgroundColor = 'lightblue';
-    }
-    if (element.dataset.isMine === '1') {
-        squares.forEach(function(element) {
-            if (element.dataset.isMine === '1') {
-                element.style.backgroundColor = 'red';
-            }
-        })
+    if (element.style.backgroundColor !== 'lightblue' && element.style.backgroundColor !== 'red') {
+        if (element.dataset.isMine === '0') {
+            element.style.backgroundColor = 'lightblue';
+            let i = parseInt(element.dataset.row,10);
+            let j = parseInt(element.dataset.column,10);
+            searchNeighbors(i,j);
+            element.innerText = element.dataset.neighbMines;
+        } else if (element.dataset.isMine === '1') {
+            squares.forEach(function(element) {
+                if (element.dataset.isMine === '1') {
+                    element.style.backgroundColor = 'red';
+                }
+            })
+        }
     }
 }
